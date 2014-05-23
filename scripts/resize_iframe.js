75774b37dec7870efb85c3b3de9ba63aa3f57f9e
Jan Stanstrup <stanstrup@gmail.com>
2014-05-23 23:33:52 +0200

    removed script

--- scripts/resize_iframe.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-  jQuery(document).ready(function () {
-    jQuery("#IDofControlFiringResizeEvent").click(function () {
-      var frame = $('#IDofiframeInMainWindow', window.parent.document);
-      var height = jQuery("#IDofContainerInsideiFrame").height();
-      frame.height(height + 15);
-    });
-  });
-</script>
