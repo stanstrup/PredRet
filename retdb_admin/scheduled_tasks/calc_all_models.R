19abef4859df51cc5c20f9af5e98595bc23843d3
Jan Stanstrup <stanstrup@gmail.com>
2014-08-28 11:02:56 +0200

    Scheduled recalculation of all models when necessary. closes #30

---
+++ retdb_admin/scheduled_tasks/calc_all_models.R
@@ -0,0 +1,19 @@
+## Basic settings ##################
+source("../settings/mongodb.R",local=TRUE)
+
+
+## functions ##################
+source("../functions/FUNCTIONS.R",local=TRUE)
+
+
+
+## Re-calc all models ##################
+sys_oids  = as.character(lapply(get_systems(),function(x) as.character.mongo.oid(x$`_id`)))
+
+system_combs = t(combn(sys_oids,2))
+system_combs = as.matrix(system_combs)
+system_combs = rbind(system_combs,system_combs[,c(2,1)])
+
+for(i in 1:nrow(system_combs)){
+  build_model(oid1=system_combs[i,1],oid2=system_combs[i,2],ns_sysmodels=ns_sysmodels,ns_rtdata=ns_rtdata,ns_sysmodels_log=ns_sysmodels_log,force=FALSE,withProgress=FALSE)
+}
