158565fdc234c41392defb8f8f40fa8384a160e9
Jan Stanstrup <stanstrup@gmail.com>
2014-11-25 09:59:28 +0100

    Confusing variables names. since the second sysoid is the one we predict to (the y in the models) we shoudl query oid_sys2 and not oid_sys1. They are the same but it is confusing.

--- retdb_admin/scheduled_tasks/make_predictions.R
+++ retdb_admin/scheduled_tasks/make_predictions.R
@@ -10,10 +10,10 @@ source("../functions/FUNCTIONS.R",local=TRUE)
 
 ## Make the predictions ##################
 models <- get_models() # change to models() if running in shiny
-sys_models_oid1 <- sapply(models,function(x) x$oid_sys1)
-sys_models_oid1_name <-  sys_oid2name(sys_models_oid1)
-sys_models_oid1_name <- sys_models_oid1_name[!duplicated(sys_models_oid1)]
-sys_models_oid1 <- sys_models_oid1[!duplicated(sys_models_oid1)]
+sys_models_oid2 <- sapply(models,function(x) x$oid_sys2)
+sys_models_oid2_name <-  sys_oid2name(sys_models_oid2)
+sys_models_oid2_name <- sys_models_oid2_name[!duplicated(sys_models_oid2)]
+sys_models_oid2 <- sys_models_oid2[!duplicated(sys_models_oid2)]
 
 
 
@@ -30,34 +30,34 @@ data_time_max <- max(sapply(data_time,function(x) x$time))
 
 
 
-for(i in 1:length(sys_models_oid1)){
+for(i in 1:length(sys_models_oid2)){
   
   # We get the time of the latest model that predicts to the system we are interested in
   # we check if:
   # 1) there is new data since last model build.
   criteria_model_time <- mongo.bson.buffer.create()
-  mongo.bson.buffer.append(criteria_model_time, "oid_sys2", sys_models_oid1[i])
+  mongo.bson.buffer.append(criteria_model_time, "oid_sys2", sys_models_oid2[i])
   criteria_model_time <- mongo.bson.from.buffer(criteria_model_time)
   model_time <- mongo.find.all(mongo,ns=ns_sysmodels,query=criteria_model_time,field=list(time=1L))
   
   model_time_max <- max(sapply(model_time,function(x) x$time))
   
   # 2) if there has ever been any data predicted
-  prev_predictions_count <- mongo.count(mongo,ns=ns_rtdata,query=list(sys_id=sys_models_oid1[i],generation=1))
+  prev_predictions_count <- mongo.count(mongo,ns=ns_rtdata,query=list(sys_id=sys_models_oid2[i],generation=1))
   
   
   if(model_time_max > data_time_max & prev_predictions_count>0) next
   
   
   # If there is any new experimental data we predict all data again.
-  predicted_data <- predict_RT(sys_models_oid1[i])
+  predicted_data <- predict_RT(sys_models_oid2[i])
   if(is.null(predicted_data)) next
   
   
   for(i2 in 1:nrow(predicted_data)){
     # We check if we already have an identical prediction. If so then we don't rewrite the entry. To avoid updating the date
     criteria_exists <- mongo.bson.buffer.create()
-    mongo.bson.buffer.append(criteria_exists, "sys_id", sys_models_oid1[i])
+    mongo.bson.buffer.append(criteria_exists, "sys_id", sys_models_oid2[i])
     mongo.bson.buffer.append(criteria_exists, "inchi", predicted_data[i2,"inchi"])
     mongo.bson.buffer.append(criteria_exists, "predicted_rt", predicted_data[i2,"predicted_rt"])
     criteria_exists <- mongo.bson.from.buffer(criteria_exists)
@@ -71,7 +71,7 @@ for(i in 1:length(sys_models_oid1)){
     bson_data = mongo.bson.from.df(predicted_data[i2,,drop=FALSE])
     
     criteria <- mongo.bson.buffer.create()
-    mongo.bson.buffer.append(criteria, "sys_id", sys_models_oid1[i])
+    mongo.bson.buffer.append(criteria, "sys_id", sys_models_oid2[i])
     mongo.bson.buffer.append(criteria, "inchi", predicted_data[i2,"inchi"])
     mongo.bson.buffer.append(criteria, "generation", 1L)
     criteria <- mongo.bson.from.buffer(criteria)
@@ -83,7 +83,7 @@ for(i in 1:length(sys_models_oid1)){
   
   # write predictions stats
   predstats <- pred_stat_make(predicted_data)
-  pred_stat_write(predstats,sys_oid=sys_models_oid1[i],ns=ns_pred_stats)
+  pred_stat_write(predstats,sys_oid=sys_models_oid2[i],ns=ns_pred_stats)
   
   
 }
