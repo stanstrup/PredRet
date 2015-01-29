b2745badc34db3ff30bf6d90b6b6807ac45c5fa6
Jan Stanstrup <stanstrup@gmail.com>
2015-01-29 11:35:37 +0100

    moved functions to chemhelper

--- _extras/tests/find_isomers_that_can_be_seperated.R
+++ _extras/tests/find_isomers_that_can_be_seperated.R
@@ -1,5 +1,7 @@
 library(stringr)
 library(Rdisop)
+library(chemhelper)
+
 
 
 # function to collapse groups
@@ -28,80 +30,6 @@ y <- unique(y)
 
 return(y)
 }
-
-
-
-
-
-
-
-
-
-
-
-diff.mat2letter.not <- function(g){
-  # http://menugget.blogspot.it/2014/05/automated-determination-of-distribution.html
-  # http://stackoverflow.com/questions/27293770/compact-letter-display-from-logical-matrix/27296818
-  require(igraph)
-  
-  
-  n <- nrow(g)
-  
-  # Re-arrange data into an "edge list" for use in igraph (i.e. which groups are "connected") - Solution from "David Eisenstat" ()
-  same <- which(g)
-  g2 <- data.frame(N1=((same-1) %% n) + 1, N2=((same-1) %/% n) + 1)
-  g2 <- g2[order(g2[[1]]),] # Get rid of loops and ensure right naming of vertices
-  g3 <- simplify(graph.data.frame(g2,directed = FALSE))
-  
-  
-  
-  # Calcuate the maximal cliques - these are groupings where every node is connected to all others
-  cliq <- maximal.cliques(g3) # Solution from "majom" ()
-  
-  # Reorder by level order - Solution from "MrFlick" ()
-  ml<-max(sapply(cliq, length))
-  reord <- do.call(order, data.frame(
-    do.call(rbind, 
-            lapply(cliq, function(x) c(sort(x), rep.int(0, ml-length(x))))
-    )
-  ))
-  cliq <- cliq[reord]
-  cliq
-  
-  # Generate labels to  factor levels
-  lab.txt <- vector(mode="list", n) # empty list
-  lab <- letters[seq(cliq)] # clique labels
-  for(i in seq(cliq)){ # loop to concatenate clique labels
-    for(j in cliq[[i]]){
-      lab.txt[[j]] <- paste0(lab.txt[[j]], lab[i])
-    }
-  }
-  
-  
-  
-  
-  return(unlist(lab.txt))
-  
-}
-
-
-
-edges2diff.mat <- function(lower,upper){
-  
-  n <- length(lower)
-  g <- outer(lower, upper,"-")
-  g <- !(g<0)
-  g <- g + t(g) # not necessary, but make matrix symmetric
-  g <- g!=1
-  rownames(g) <- 1:n # change row names
-  colnames(g) <- 1:n # change column names
-  
-  return(g)
-}
-
-
-
-
 
 
 
