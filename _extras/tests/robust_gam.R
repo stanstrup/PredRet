584406ffbae0b7469f92fc0a55113b903ee4e9c2
Jan Stanstrup <stanstrup@gmail.com>
2015-02-25 20:51:32 +0100

    trying robust GAM

---
+++ _extras/tests/robust_gam.R
@@ -0,0 +1,56 @@
+library(mgcv)
+
+# get data
+# comb_matrix = sys_comb_matrix(oid1 = unique(data$sys_id[data$system=="RIKEN"]),oid2 = unique(data$sys_id[data$system=="FEM_long"]),ns=ns_rtdata)
+# del = as.vector(apply(comb_matrix$rt,1,function(x) any(is.na(x))))
+# comb_matrix$rt = comb_matrix$rt[!del,,drop=F]
+# comb_matrix$inchi = comb_matrix$inchi[!del,drop=F]
+# 
+# ord = order(comb_matrix$rt[,1])
+# comb_matrix$rt = comb_matrix$rt[ord,,drop=F]
+# comb_matrix$inchi = comb_matrix$inchi[ord,drop=F]
+
+
+comb_matrix <- readRDS("test.dat.rds")
+
+
+
+
+
+
+
+plot(comb_matrix$rt)
+
+
+# standard gam with monotonic contraint
+newdata = seq(from=min(comb_matrix$rt[,1]),to=max(comb_matrix$rt[,1]),length.out=1000)
+x.star <- comb_matrix$rt[,1]
+y.star <- comb_matrix$rt[,2]
+
+dat <- data.frame(x=x.star,y=y.star)
+f.ug <- gam(y~s(x,k=min(length(unique(x.star)),10),bs="tp"),data=dat)
+sm <- smoothCon(s(x,k=min(length(unique(x.star)),10),bs="cr"),dat,knots=NULL)[[1]]
+con <- mono.con(sm$xp);   # get constraints
+G <- list(X=sm$X,C=matrix(0,0,0),sp=f.ug$sp,p=sm$xp,y=y.star,w=y.star*0+1)
+G$Ain <- con$A
+G$bin <- con$b
+G$S <- sm$S
+G$off <- 0
+
+p <- pcls(G);  # fit spline (using s.p. from unconstrained fit)
+
+fv<-Predict.matrix(sm,data.frame(x=newdata))%*%p
+
+y_pred <- as.numeric(fv)
+y_pred[y_pred < 0] = 0
+
+
+lines(newdata,y_pred)
+
+
+
+# robust
+
+f.ug2 <- DoubleRobGam(y~bsp(x),data=dat)
+lines(dat$x,f.ug2$fitted.values,col="red")
+
