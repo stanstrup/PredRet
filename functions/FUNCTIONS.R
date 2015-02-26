49309e9df6204ce3c388c6a894892155750857c0
Jan Stanstrup <stanstrup@gmail.com>
2015-02-26 15:34:27 +0100

    dependency added.

--- functions/FUNCTIONS.R
+++ functions/FUNCTIONS.R
@@ -518,6 +518,7 @@ loess.fun <- function(in_data,inds,newdata,span){
 
 gam.mono.con.fun <- function(in_data,inds,newdata){
   require(mgcv)
+  require(pracma)
   x.star <- in_data[,1][inds]
   y.star <- in_data[,2][inds]
   
