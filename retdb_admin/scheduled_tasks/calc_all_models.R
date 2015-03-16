7e828ee43246a6dee9d8a3e350e13b9a6d390a8c
Jan Stanstrup <stanstrup@gmail.com>
2015-03-16 15:59:42 +0100

    Only make models between systems with same column type

--- retdb_admin/scheduled_tasks/calc_all_models.R
+++ retdb_admin/scheduled_tasks/calc_all_models.R
@@ -8,12 +8,31 @@ source("../functions/FUNCTIONS.R",local=TRUE)
 
 
 ## Re-calc all models ##################
-sys_oids  = as.character(lapply(get_systems(),function(x) as.character.mongo.oid(x$`_id`)))
+systems <- get_systems()
 
+systems <- lapply(systems,function(x) {
+                                          y <- x
+                                          y$`_id` <- as.character.mongo.oid(y$`_id`)
+                                          return(y)  
+                                      }
+                  )
+
+systems <- data.frame(do.call(rbind, systems), stringsAsFactors=FALSE)
+systems <- as.data.frame(lapply(systems,unlist), stringsAsFactors=FALSE)
+
+
+sys_oids  = systems$X_id
 system_combs = t(combn(sys_oids,2))
 system_combs = as.matrix(system_combs)
 system_combs = rbind(system_combs,system_combs[,c(2,1)])
 
+
+
+
 for(i in 1:nrow(system_combs)){
+  
+  # if different column types don't make models
+  if(    systems[system_combs[i,1]==systems$X_id, "system_column_type"]   !=  systems[system_combs[i,2]==systems$X_id, "system_column_type"]       )   next
+  
   build_model(oid1=system_combs[i,1],oid2=system_combs[i,2],ns_sysmodels=ns_sysmodels,ns_rtdata=ns_rtdata,ns_sysmodels_log=ns_sysmodels_log,force=FALSE,withProgress=FALSE)
 }
