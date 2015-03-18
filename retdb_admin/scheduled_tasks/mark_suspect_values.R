541dc39bcb210be02f6f134ddf5734b9a8bfdb77
Jan Stanstrup <stanstrup@gmail.com>
2015-03-18 10:08:14 +0100

    Re-written logic for marking suspect values.

--- retdb_admin/scheduled_tasks/mark_suspect_values.R
+++ retdb_admin/scheduled_tasks/mark_suspect_values.R
@@ -8,6 +8,8 @@ source("../functions/FUNCTIONS.R",local=TRUE)
 
 
 
+
+
 ## get data needed ##################
 rtdata <- get_user_data(ns=ns_rtdata,generation=0)
 
@@ -16,32 +18,44 @@ models_oid_sys1 <- sapply(models,function(x) x$oid_sys1)
 models_oid_sys2 <- sapply(models,function(x) x$oid_sys2)
 
 
+unique_inchi <- as.matrix(table(rtdata$inchi))
+unique_inchi <- rownames(unique_inchi)[unique_inchi>1]
 
 
 
-## Run through all entries ##################
-suspects_initial <- list()
 
-for(i in 1:nrow(rtdata)){
+
+
+## Go through all inchis and locate suspect values ##################
+suspect <- data.frame(matrix(ncol=2,nrow=0))
+colnames(suspect) <- c("sysid","inchi")
+
+
+for(i in 1:length(unique_inchi)){
   
-  ## Get the models where the rt could have been used.
-  # the system of the entry
-  entry_sys_id <- rtdata[i,"sys_id"]
+  # Make comparison table for the inchi
+  # The table will say in which models one of the values is suspect
+  select <- rtdata$inchi==unique_inchi[i]
+  sys_select <- rtdata$sys_id[select]
+  
+  bad_mat <- matrix(ncol=sum(select),nrow=sum(select))
+  colnames(bad_mat) <- rownames(bad_mat) <- sys_select
   
   
   
-  # run through all models to see if the entry is used in any of them
-  model_select <- which(entry_sys_id==models_oid_sys1 | entry_sys_id==models_oid_sys2) # is the system from the entry used in any model?
-  if(length(model_select)==0){next}
-  
-  suspects_initial[[i]] <- list()
-  for(i2 in model_select){
-    
-    
-    xy_mat <- models[[i2]]$xy_mat
-    
-    entry_select  <-   xy_mat$inchi==rtdata[i,"inchi"]
-    if(!any(entry_select)){next} # is the inchi of the entry used in the model?
+  # Go through the matrix point by point and fill in the info
+  for(row in 1:nrow(bad_mat)){
+    for(col in 1:ncol(bad_mat)){
+      
+      model_select <- models_oid_sys1 == rownames(bad_mat)[row] & models_oid_sys2 == colnames(bad_mat)[col]
+      if(all(!model_select)) next
+      
+      
+      # Get the model      
+      xy_mat <- models[[which(model_select)]]$xy_mat
+      
+      entry_select  <-   xy_mat$inchi==unique_inchi[i]
+      if(!any(entry_select)){next} # is the inchi of the entry used in the model?
       
       
       # original values. should be same as in rtdata[i,"inchi"]
@@ -50,8 +64,8 @@ for(i in 1:nrow(rtdata)){
       
       
       # predicted value in this model
-      pred_select <- which.min(abs(models[[i2]]$newdata-x_org))
-      ci <- models[[i2]]$ci[pred_select,]
+      pred_select <- which.min(abs(models[[which(model_select)]]$newdata-x_org))
+      ci <- models[[which(model_select)]]$ci[pred_select,]
       ci <- as.data.frame(t(sapply(ci,unlist)))
       
       # see if outside CI*suspect_CI_multiplier
@@ -59,120 +73,81 @@ for(i in 1:nrow(rtdata)){
       ci_lower_lim <- with(ci,     pred-(pred-lower)*suspect_CI_multiplier        )
       
       
-      if(!(y_org>ci_upper_lim | y_org<ci_lower_lim )){next} # if inside limits just go on
-        
-        
-    
-    suspects_initial[[i]][[i2]] <- data.frame(index = i , oid_sys1 = models[[i2]]$oid_sys1,oid_sys2 = models[[i2]]$oid_sys2)
+      if(!(y_org>ci_upper_lim | y_org<ci_lower_lim )){
+        bad_mat[row,col]=FALSE# if inside limits
+      }else{bad_mat[row,col]=TRUE # if outside limits
+      } 
       
-
+    }
   }
-
-}
-
-
-
-# function to remove NULL and zero length entries in a list
-clean_list <- function(list){
-  list_clean <- list[!sapply(list,function(x) is.null(x) | length(x)==0)]
-  return(list_clean)
-}
-
-
-# Remove all NULL and zero length values (here the loop was skipped because the entry system was never used in a model or the entry was never used in a model)
-suspects_initial_clean <- clean_list(suspects_initial)
-suspects_initial_clean <- lapply(suspects_initial_clean,clean_list)
-suspects_initial_clean <- suspects_initial_clean[sapply(suspects_initial_clean,length)>0]
-
-
-#Making each entry data a data.frame
-suspects_initial_clean <- lapply(suspects_initial_clean,function(x) do.call(rbind,x))
-
-
-# Order the ids rowwise. this means that if 2 rows contain the same two sysids the rows are not identical.
-suspects_initial_clean <- lapply(suspects_initial_clean,function(x){
-  sorted = apply(x,1,function(y){
-    order = order(y[c(2,3)])
-    out <- y
-    out[c(2,3)] <- out[c(2,3)][order]
-    return(t(out)) 
-  }                                                
-)
-
-return(as.data.frame(t(sorted),stringsAsFactors=F))
-}
-
-)
+  
+  
+  
+  
+  # if noone is suspect go on to the next inchi
+  if(all(!bad_mat,na.rm = TRUE)){next} 
+  
+  
+  
+  # If there are suspect values but never in more than system combination
+  # Then we can never know who is to blame so we mark both of them
+  # If systems exist both ways we only consider it a suspect value if it is suspect both ways
+  if(!(any(rowSums(bad_mat,na.rm = TRUE) > 1) | any(colSums(bad_mat,na.rm = TRUE) > 1))){
+    bad_mat_entries <- which(bad_mat,arr.ind = TRUE)
+    
+    for(i2 in 1:nrow(bad_mat_entries)){
+#      if(     bad_mat[bad_mat_entries[i2,1],bad_mat_entries[i2,2]]      &      bad_mat[bad_mat_entries[i2,2],bad_mat_entries[i2,1]]             ){
+        suspect[(nrow(suspect)+1):(nrow(suspect)+2),] <- data.frame(sysid=sys_select[bad_mat_entries[i2,]],inchi=unique_inchi[i],stringsAsFactors=F)
+#      }
+    }
+    
+  }
+  
+  
+  # If there are suspect values but some values are suspect in more than system combination
+  # then we can try to pinpoint who is to blame
+  if((any(rowSums(bad_mat,na.rm = TRUE) > 1) | any(colSums(bad_mat,na.rm = TRUE) > 1))){
+    bad_mat_entries <- which(rowSums(bad_mat,na.rm = TRUE) > 1 | colSums(bad_mat,na.rm = TRUE) > 1)
+    suspect[(nrow(suspect)+1):(nrow(suspect)+length(bad_mat_entries)),] <- data.frame(sysid=sys_select[bad_mat_entries],inchi=unique_inchi[i],stringsAsFactors=F)
+  }
+  
+  
+  
+} 
 
 
 
-# Remove repeated rows
-suspects_initial_clean <- lapply(suspects_initial_clean,unique)
-
-# Get count of appearence of each system where entry is suspect
-suspects_initial_clean_counts <- lapply(suspects_initial_clean,function(x) as.matrix(table(as.vector(as.matrix(x[,c(2,3)])))))
-
-
-# Check when the entry is suspect in more than one system combination
-n <- 0 # at least in one system. less safe and cannot pinpoint which system is to blame
-suspects_initial_clean_hit <- sapply(suspects_initial_clean_counts,function(x) any(x>n))
-
-
-
-final_suspects <- as.numeric(unique(do.call(rbind.data.frame,suspects_initial_clean[suspects_initial_clean_hit])[,1]))
+suspect <- unique(suspect)
 
 
 
 
 
-# Set all to false. this is just to make the new field available. Done ones
-# rtdata <- get_user_data(ns=ns_rtdata)
-# ids <- rtdata$`_id`
-# 
-# mongo <- mongo.create()
-# 
-# for(i in 1:nrow(rtdata)){
-#   
-#   buf <- mongo.bson.buffer.create()
-#   mongo.bson.buffer.append(buf, "_id", mongo.oid.from.string(ids[i]))
-#   criteria <- mongo.bson.from.buffer(buf)
-#   
-#   suspect = list()
-#   suspect[["suspect"]] = FALSE
-#   mongo.update(mongo, ns=ns_rtdata, criteria,   list('$set'=suspect)      )
-#   
-# }
-# 
-# del <- mongo.disconnect(mongo)
-# del <- mongo.destroy(mongo) 
+
+# db connection
+mongo <- mongo.create()
 
 
+# Reset suspect list
+#mongo.update(mongo, ns=ns_rtdata, criteria=mongo.bson.empty(),   list('$set'=list(suspect=FALSE))  ,flags=2L    )
 
 
 
 # write results to db. all records updated
-mongo <- mongo.create()
-
-for(i in 1:nrow(rtdata)){
+for(i in 1:nrow(suspect)){
+  criteria <- list(sys_id=suspect$sysid[i]  ,inchi=suspect$inchi[i] ,generation=0)
   
-  buf <- mongo.bson.buffer.create()
-  mongo.bson.buffer.append(buf, "_id", mongo.oid.from.string(rtdata[i,"_id"]))
-  criteria <- mongo.bson.from.buffer(buf)
+  old_suspect <- mongo.find.all(mongo, ns=ns_rtdata, query=criteria,fields=list(suspect = 1L))
+  old_suspect <- old_suspect[[1]]$suspect
   
-  
-  suspect = list()
-  
-  if(any(final_suspects==i)){
-  suspect[["suspect"]] = TRUE
-  }else{
-    suspect[["suspect"]] = FALSE
+  if(!old_suspect){
+  mongo.update(mongo, ns=ns_rtdata, criteria=criteria,  objNew = list(  '$set'=list(suspect=TRUE,time = Sys.time())     )     )
   }
   
-  mongo.update(mongo, ns=ns_rtdata, criteria,   list('$set'=suspect)      )
   
 }
 
+
+
 del <- mongo.disconnect(mongo)
 del <- mongo.destroy(mongo) 
-
-
