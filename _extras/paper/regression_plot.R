3a08ac24b5a8b1637ea5730bf074a7201986bb17
Jan Stanstrup <stanstrup@gmail.com>
2015-03-18 10:04:33 +0100

    Regression plot back to linear scale. error plots don't need ylim anymore since there are no heavy outliers.

--- _extras/paper/regression_plot.R
+++ _extras/paper/regression_plot.R
@@ -20,8 +20,8 @@ data_sub <- data[select,]
 
 
 # correlation plot
-reg_model <- lm(data_sub$predicted_rt ~ data_sub$recorded_rt)
-rreg_model <- rlm(data_sub$predicted_rt ~ data_sub$recorded_rt)
+reg_model <- lm(predicted_rt ~ recorded_rt,data=data_sub)
+rreg_model <- rlm(predicted_rt ~ recorded_rt,data=data_sub)
 summary(reg_model)$r.squared
 r2(rreg_model)
 
@@ -31,14 +31,14 @@ p <- ggplot(df, aes(x = x, y = y, ymin = down, ymax = up,label=text) )
 p <- p + geom_ribbon(fill = 'grey80', alpha = 1)
 p <- p +   geom_point(color = 'black', linetype = 'dashed')
 p <- p + theme_bw()
-p <- p + scale_x_log10() 
-p <- p + scale_y_log10(limits = c(0.1,max(df[,"y"]))) 
-p <- p + annotation_logticks()
+# p <- p + scale_x_log10() 
+# p <- p + scale_y_log10(limits = c(0.1,max(df[,"y"]))) 
+# p <- p + annotation_logticks()
 #p <- p + geom_abline(intercept=0, slope=1) 
 #p <- p + geom_abline(data=data.frame(a = coef(rreg_model)[1],b = coef(rreg_model)[2]), aes(intercept=a, slope=b),color="red")
 p <- p + geom_abline(data=data.frame(a = coef(reg_model)[1],b = coef(reg_model)[2]), aes(intercept=a, slope=b),color="blue")
-p <- p + annotate("text",x=0,y=20,hjust=-0.2,vjust=0,label= paste0("R^{2}==",round(summary(reg_model)$r.squared,3) ),parse = TRUE         )
-p <- p + geom_text(hjust=-0.2, vjust=0.3)
+p <- p + annotate("text",x=0,y=60,hjust=-0.2,vjust=0,label= paste0("R^{2}==",round(summary(reg_model)$r.squared,3) ),parse = TRUE         )
+#p <- p + geom_text(hjust=-0.2, vjust=0.3)
 
 plot(p)
 
@@ -48,10 +48,10 @@ plot(p)
 
 
 # error
-plot( data_sub$recorded_rt,       (data_sub$predicted_rt-data_sub$recorded_rt)   ,pch=20,ylim=c(-5,5))
+plot( data_sub$recorded_rt,       (data_sub$predicted_rt-data_sub$recorded_rt)   ,pch=20)
 
 
 # rel error
-plot( data_sub$recorded_rt,       (data_sub$predicted_rt-data_sub$recorded_rt)/data_sub$recorded_rt    ,ylim=c(-1,1) ,pch=20)
+plot( data_sub$recorded_rt,       (data_sub$predicted_rt-data_sub$recorded_rt)/data_sub$recorded_rt    ,pch=20)
 
 
