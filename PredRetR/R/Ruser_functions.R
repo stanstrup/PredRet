35a03469924f1bca0a35ec88ac45bbc0e6e4ca76
Jan Stanstrup <stanstrup@gmail.com>
2015-05-08 15:33:03 +0200

    1) added PredRet_get_models 3) added man pages for user functions

--- PredRetR/R/Ruser_functions.R
+++ PredRetR/R/Ruser_functions.R
@@ -41,7 +41,10 @@ PredRet_get_db <- function(userID=NULL,exp_pred = c("exp","pred"), include_suspe
 # Get systems
 PredRet_get_chrom_systems <- function(){
   sys <- get_systems()
-  for(i in 1:length(sys)) sys[[i]]$`_id`=NULL
+  for(i in 1:length(sys)){
+    sys[[i]]$`_id`=NULL
+    sys[[i]]$userID=NULL
+  }
   
   return(sys)
 }
@@ -49,4 +52,26 @@ PredRet_get_chrom_systems <- function(){
 
 
 # Get models
-
+PredRet_get_models <- function(){
+  models <- get_models(include.ci=TRUE,include.newdata=TRUE,include.xy_mat=TRUE)
+  
+  for(i in 1:length(models)){
+    
+    imodel <- list()
+    imodel$predict_from <- sys_oid2name(models[[i]]$oid_sys1)
+    imodel$predict_to   <- sys_oid2name(models[[i]]$oid_sys2)
+    imodel$model_fit    <- data.frame(x = models[[i]]$newdata, models[[i]]$ci)
+    imodel$model_points <- models[[i]]$xy_mat
+    names(imodel$model_points)[1:2] <- c(imodel$predict_from,imodel$predict_to)
+    imodel$stats        <- unlist(models[[i]][c("n_points","mean_error_abs","median_error_abs","q95_error_abs",
+                                                "max_error_abs","mean_ci_width_abs","median_ci_width_abs",
+                                                "q95_ci_width_abs","max_ci_width_abs")
+                                              ]
+                                  )
+    
+    models[[i]] <- imodel
+  }
+  
+  return(models)
+  
+}
