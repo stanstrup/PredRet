49dd94cad243740d344bc5e8a0435ffbcb3c0288
Jan Stanstrup <stanstrup@gmail.com>
2015-08-20 14:02:07 +0200

    Tried to weigh retention times by predicted logP. Doesn't work either. closes #42

--- _extras/structure_based/similarity.R
+++ _extras/structure_based/similarity.R
@@ -66,33 +66,33 @@ db_support <- lapply(1:nrow(db), function(i){
 
 
 # RT weighted by similary
-db$RT_pred <- sapply(db_support,function(x){
+db$RT_pred_sim_weight <- sapply(db_support,function(x){
   w <- sigmoid(x$sim, a = 50, b = 0.95)
   weighted.mean(x$RT, w,na.rm =T)
   })
 
 
 # choosing just the RT from the most similar compound
-db$RT_pred2 <- sapply(db_support,function(x){
+db$RT_pred_closest <- sapply(db_support,function(x){
   x$RT[which.max(x$sim)]
 })
 
 
 
-db <- mutate(db, error_abs =  abs(RT_pred-recorded_rt) ,  error_rel =   abs(RT_pred-recorded_rt)/recorded_rt, error_rel2 =   abs(RT_pred2-recorded_rt)/recorded_rt  )
+db <- mutate(db, sim_weight_error_abs =  abs(RT_pred_sim_weight-recorded_rt) ,  sim_weight_error_rel =   abs(RT_pred_sim_weight-recorded_rt)/recorded_rt, closest_error_rel =   abs(RT_pred_closest-recorded_rt)/recorded_rt  )
 
 
 stats <- 
   ddply(db, .(system), summarise, 
-        N                        = sum(!is.na(RT_pred)),
-        error_median_abs         = median(    error_abs   ,na.rm = T ),
-        error_median_rel         = median(    error_rel     ,na.rm = T),
-        error_median_rel2        = median(    error_rel2     ,na.rm = T)
+        N                        = sum(!is.na(RT_pred_sim_weight)),
+        sim_weight_error_abs         = median(    sim_weight_error_abs   ,na.rm = T ),
+        sim_weight_error_rel         = median(    sim_weight_error_rel     ,na.rm = T),
+        closest_error_rel        = median(    closest_error_rel     ,na.rm = T)
   )
         
 
 
-plot(stats$error_median_rel,stats$error_median_rel2)
+plot(stats$sim_weight_error_rel,stats$closest_error_rel)
 abline(a=0,b=1)
 
 
@@ -108,4 +108,48 @@ db$w_sum <- sapply(db_support,function(x){
 })
 
 
-plot(db$w_sum,db$error_rel,pch=20)
+plot(db$w_sum,db$sim_weight_error_rel,pch=20)
+
+
+
+
+# using descriptors as weights
+
+desc <- c("org.openscience.cdk.qsar.descriptors.molecular.XLogPDescriptor",
+          "org.openscience.cdk.qsar.descriptors.molecular.TPSADescriptor",
+          "org.openscience.cdk.qsar.descriptors.molecular.MannholdLogPDescriptor",
+          "org.openscience.cdk.qsar.descriptors.molecular.APolDescriptor",                     
+          "org.openscience.cdk.qsar.descriptors.molecular.ALOGPDescriptor"
+         )
+
+
+desc_out <- eval.desc(mols, desc)
+plot(desc_out)
+db <- cbind.data.frame(db,desc_out)
+
+
+desc_range <- diff(range(db$MLogP))
+desc_mat <- outer(db$MLogP,db$MLogP, function(x,y) desc_range/abs(x-y)   )
+
+desc_mat[is.infinite(desc_mat)] <- NA
+
+
+
+# RT weighted by LogP
+for(i in 1:nrow(db)){
+ 
+  select <- which(db$system[i]==db$system) # get all other molecules from same system
+  select <- select[ -which(db$inchi[i] == db$inchi[select])] # remove itself
+  
+  w <- desc_mat[select,i]
+   w <- sigmoid(w, a = 10, b = 50)
+db$RT_pred_desc_weight[i] <- weighted.mean(db$recorded_rt[select], w,na.rm =T)
+  
+}
+
+
+
+  ddply(db, .(system), summarise, 
+        error_rel        = median(     abs(RT_pred_desc_weight-recorded_rt)/recorded_rt     ,na.rm = T)
+  )
+
