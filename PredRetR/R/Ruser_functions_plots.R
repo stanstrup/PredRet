4eb3b7314deee14c524e23df9ad3cbda9fd9ea8d
Jan Stanstrup <stanstrup@gmail.com>
2015-09-03 14:33:49 +0200

    just some text alignment

--- PredRetR/R/Ruser_functions_plots.R
+++ PredRetR/R/Ruser_functions_plots.R
@@ -22,16 +22,16 @@ db.stats <- function(data){
   predicted_rt <- recorded_rt <- error_abs <- select <- error_rel <- ci_width_abs <- ci_width_rel <- NULL  # making package check happy
   
   stats <- ddply(data, .(system), summarise,
-                 N         = sum(!is.na(predicted_rt)),                                      # predictions
-                 N_ex      = sum(!is.na(predicted_rt) & is.na(recorded_rt))            ,     # predictions with unknown RT
+                 N                = sum(!is.na(predicted_rt)),                               # predictions
+                 N_ex             = sum(!is.na(predicted_rt) & is.na(recorded_rt))   ,       # predictions with unknown RT
                  error_abs_median = median(error_abs[select])        ,                       # abs error
                  error_rel_median = median(error_rel[select])        ,                       # abs error
-                 error_abs = mean(error_abs[select])        ,                                # abs error
-                 error_rel = mean(error_rel[select])        ,                                # rel error
-                 ci_abs = mean(ci_width_abs,na.rm = T)        ,                                                       
-                 ci_rel = mean(ci_width_rel,na.rm = T)        ,   
-                 ci_abs_median = median(ci_width_abs,na.rm = T)        ,
-                 ci_rel_median = median(ci_width_rel,na.rm = T)        ,
+                 error_abs        = mean(error_abs[select])        ,                         # abs error
+                 error_rel        = mean(error_rel[select])        ,                         # rel error
+                 ci_abs           = mean(ci_width_abs,na.rm = T)        ,                                                       
+                 ci_rel           = mean(ci_width_rel,na.rm = T)        ,   
+                 ci_abs_median    = median(ci_width_abs,na.rm = T)        ,
+                 ci_rel_median    = median(ci_width_rel,na.rm = T)        ,
                  .drop=F)
   
   return(stats)
