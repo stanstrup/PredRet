a841b138f00a7cbb7ca5e1ee851a8ff4a3ff066a
Jan Stanstrup <stanstrup@gmail.com>
2015-10-26 14:45:19 +0100

    Cleaned up script and made it run out of the box.

--- _extras/structure_based/PredRet_QSRR/QSRR_all_systems_separately.R
+++ _extras/structure_based/PredRet_QSRR/QSRR_all_systems_separately.R
@@ -1,9 +1,12 @@
+# Change working dir ------------------------------------------------------
 setwd("_extras/structure_based/PredRet_QSRR/")
 
+
+# Load packages -----------------------------------------------------------
 library(plyr)
 library(dplyr)
 library(tidyr)
-library(Rplot.extra)
+library(Rplot.extra) # devtools::install_github("stanstrup/Rplot.extra")
 library(parallel)
 library(caret)
 library(doParallel)
@@ -11,15 +14,25 @@ library(qdapTools)
 library(magrittr)
 
 
-#load("dataset_all_with_pred.RData)
+# Load the database (can also be pulled directly) -------------------------
+load("dataset_all_with_pred.RData")
+load("camb functions.RData") # since the camb package doesn't run on windows
 
+
+
+
+# Set loop to run through all systems -------------------------------------
 systems <- unique(dataset$system)
 systems <- expand.grid(pred = c(FALSE,TRUE),systems = systems)
 models <- list()
 
 
 
-cl <- makePSOCKcluster(6)
+
+# Make models for all systems (this will take some time) ------------------
+#loadRDS(models, file = "models.rds") # To save time you can load the data from here too
+
+cl <- makePSOCKcluster(detectCores()-1)
 registerDoParallel(cl)
 
 
@@ -49,17 +62,23 @@ for(i in 1:nrow(systems)){
 
 stopCluster(cl)
 
-saveRDS(models, file = "models.rds")
-
-
-
-
-plot(models[[i]], metric = "RMSE")
+#saveRDS(models, file = "models.rds")
 
 
 
 
 
+
+# RMSE plots for the models -----------------------------------------------
+plot(models[[1]], metric = "RMSE") # remember to change the number to select the model you want
+
+
+
+
+
+
+
+# Make data for plotting --------------------------------------------------
 plotdata <- lapply(1:length(models), function(i) {
   
   if(class(models[[i]])=="try-error") return(NULL)
@@ -84,6 +103,8 @@ plotdata <- do.call(rbind.data.frame,plotdata)
 
 
 
+
+# Make a plot of the accuracy ---------------------------------------------
 theme_common <- list(
   theme(axis.title.y = element_text(hjust=0.45,vjust=1)   ),
   theme(axis.title.x = element_text(vjust=4)    ),
@@ -102,7 +123,7 @@ p <- ggplot( plotdata, aes( x = preds, y = error_rel ) )
 #p <- p + scale_x_discrete(breaks=levels(interaction(plotdata$system,plotdata$preds)), drop=FALSE)
 p <- p + scale_y_continuous(breaks = seq(0, 50, 0.2),limits=c(0,2))
 p <- p + geom_violin(trim=TRUE, adjust=0.3,scale="width",size=0,width=violin_width)
-p <- p + labs(title="Absolute prediction errors", y="Error",fill="Quartiles")
+p <- p + labs(title="Relative prediction errors", y="Error",fill="Quartiles")
 p <- p + theme_bw_nice + theme_common
 #p <- p + geom_violin_quantile_fill(p=p,df_gr = interaction(plotdata[,"preds"],plotdata[,"system"]),df_data = plotdata[,"error_abs"],width=violin_width)
 p <- p + scale_fill_manual(values = c("lightgrey","darkgrey","darkgrey","lightgrey"),labels = c("25", "50", "75","100"))
@@ -117,8 +138,5 @@ p <- p + theme(legend.position=c(1,1),legend.justification=c(1,1),
 
 p <- p + labs(x="System")
 p
-
-
-
 
 
