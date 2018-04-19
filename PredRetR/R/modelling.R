2699ff4f1560c0d585c661e9993b9b67ac0fb486
Jan Stanstrup <stanstrup@gmail.com>
2018-04-19 11:37:48 +0200

    Modelling bug which discarded perfectly good predictions at small RTs
    Predictions were required to have CI width less than 2 min AND less than 20% of the predicted RT. This seems weird and was not the intention as described in the paper:
    *"Predictions with PI width larger than 2 min or more than 20% of the predicted RT are discarded as unreliable. Predictions"*

--- PredRetR/R/modelling.R
+++ PredRetR/R/modelling.R
@@ -446,7 +446,7 @@ predict_RT <- function(predict_to_system) {
     ci_width <- single_inchi_data$ci_upper-single_inchi_data$ci_lower
     ci_width_rel <-     ci_width   /   single_inchi_data$predicted
     
-    select <- ci_width < PredRet.env$prediction$ci_width_limit & ci_width_rel < PredRet.env$prediction$ci_width_limit_rel
+    select <- ci_width < PredRet.env$prediction$ci_width_limit | ci_width_rel < PredRet.env$prediction$ci_width_limit_rel
     
     if (!any(select)) next
     single_inchi_data <- single_inchi_data[select,,drop=FALSE]
